c74030df1d5875ff234dc23c85682a5776f55eb8
KHasanAbdukarimov <abdukarimovhasan4@gmail.com>
2024-06-20 11:59:49 +0500

    Added TTD

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+"""
+Calculator functions
+"""
+
+
+
+def add(x,y):
+    """Add x and y and return results."""
+    return x + y
+
+
+def subtract(y,x):
+    """Add x and y and return results."""
+    return x - y
